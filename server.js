2feeda2bbfec504bc0e41c97376c8294494244ae
Florim Sabani <mail@mail.de>
2022-03-10 02:17:01 +0100

    added online functionality

---
+++ server.js
@@ -0,0 +1,25 @@
+const io = require("socket.io")(3000, {
+  cors: {
+    origin: ['http://localhost:5501']
+  }
+});
+
+io.on('connection', socket => {
+  console.log(socket.id);
+  socket.on('newStroke', newStroke => {
+    socket.broadcast.emit('newStroke', newStroke);
+  });
+
+  socket.on('playerState', player => {
+    socket.broadcast.emit('playerState', player);
+  })
+
+  socket.on('cameraPos', camera => {
+    socket.broadcast.emit('cameraPos', camera);
+  })
+
+  socket.on('undo', () => {
+    socket.broadcast.emit('undo');
+  });
+});
+
